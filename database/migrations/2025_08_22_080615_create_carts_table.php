e84a57f22917110c23974603a1ea2bc83214f7de
M-J-Christina213 <m.j.christina213@gmail.com>
2025-08-23 08:30:41 +0530

    created the fields for cat table linking with user id

--- database/migrations/2025_08_22_080615_create_carts_table.php
+++ database/migrations/2025_08_22_080615_create_carts_table.php
@@ -11,8 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('carts', function (Blueprint $table) {
+       Schema::create('carts', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade'); 
+            $table->foreignId('menu_id')->constrained('menus')->onDelete('cascade');
+            $table->integer('quantity')->default(1);
             $table->timestamps();
         });
     }
