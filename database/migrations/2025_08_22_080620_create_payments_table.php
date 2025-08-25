8f7685dfad566cdf522c55dbbc4762a232f10654
M-J-Christina213 <m.j.christina213@gmail.com>
2025-08-25 15:55:02 +0530

    created payments fields for the table 💳

--- database/migrations/2025_08_22_080620_create_payments_table.php
+++ database/migrations/2025_08_22_080620_create_payments_table.php
@@ -14,11 +14,13 @@ return new class extends Migration
         Schema::create('payments', function (Blueprint $table) {
             $table->id();
             $table->foreignId('order_id')->constrained()->onDelete('cascade');
-            $table->string('payment_method');
+            $table->enum('payment_method', ['cash', 'card', 'stripe', 'paypal']);
             $table->decimal('amount', 8, 2);
-            $table->string('transaction_id')->unique();
+            $table->string('transaction_id')->unique()->nullable();
+            $table->enum('status', ['pending', 'successful', 'failed', 'refunded'])->default('pending');
             $table->timestamps();
         });
+
     }
 
     /**
