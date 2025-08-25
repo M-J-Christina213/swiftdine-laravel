22176ec082d2eb9046b8a94129d9333d9a51267e
M-J-Christina213 <m.j.christina213@gmail.com>
2025-08-25 16:00:35 +0530

    created the rest, resevations, staff, supplier tables

--- database/migrations/2025_08_22_080621_create_reservations_table.php
+++ database/migrations/2025_08_22_080621_create_reservations_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('reservations', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('restaurant_id')->constrained()->onDelete('cascade');
+            $table->dateTime('date_time');
+            $table->enum('status', ['pending', 'confirmed', 'cancelled'])->default('pending');
             $table->timestamps();
         });
     }
